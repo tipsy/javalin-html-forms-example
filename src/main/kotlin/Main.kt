f804ba109b4f4d0837e17c17b80b7bcc971bbc14
David <davidaase@hotmail.com>
2017-07-31 16:40:45 +0200

    Add kotlin version

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import io.javalin.Javalin
+import org.apache.commons.io.FileUtils
+import java.io.File
+
+val reservations = mutableMapOf<String?, String?>(
+        "saturday" to "No reservation",
+        "sunday" to "No reservation"
+)
+
+fun main(args: Array<String>) {
+
+    val app = Javalin.create()
+            .port(7777)
+            .enableStaticFiles("/public")
+            .start()
+
+    app.post("/make-reservation") { ctx ->
+        reservations[ctx.formParam("day")] = ctx.formParam("time")
+        ctx.html("Your reservation has been saved")
+    }
+
+    app.get("/check-reservation") { ctx ->
+        ctx.html(reservations[ctx.queryParam("day")]!!)
+    }
+
+    app.post("/upload-example") { ctx ->
+        ctx.uploadedFiles("files").forEach { (_, content, name) ->
+            FileUtils.copyInputStreamToFile(content, File("upload/" + name))
+        }
+        ctx.html("Upload complete")
+    }
+
+}
