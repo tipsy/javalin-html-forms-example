fbb1462cc8c808429725880cd673f54bef4654b8
David <davidaase@hotmail.com>
2017-08-30 20:07:01 +0200

    Bumpit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.commons.io.FileUtils;
 
 import io.javalin.Javalin;
+import io.javalin.LogLevel;
 
 public class Main {
 
@@ -18,6 +19,7 @@ public class Main {
 
         Javalin app = Javalin.create()
             .port(7777)
+            .requestLogLevel(LogLevel.EXTENSIVE)
             .enableStaticFiles("/public")
             .start();
 
