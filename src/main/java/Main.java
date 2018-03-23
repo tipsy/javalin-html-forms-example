9d674268ae765840e0a0dd8e102e071dadceb0f1
David <davidaase@hotmail.com>
2018-03-23 16:42:36 +0100

    bump

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,6 @@ public class Main {
             .start();
 
         app.post("/make-reservation", ctx -> {
-            ctx.queryParam("test");
             reservations.put(ctx.formParam("day"), ctx.formParam("time"));
             ctx.html("Your reservation has been saved");
         });
